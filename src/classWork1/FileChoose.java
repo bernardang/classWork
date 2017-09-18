2987e588328a6819c41655901da648b56d101236
Bernard <bcang@iastate.edu>
2017-09-18 00:53:29 -0500

    updated

--- src/classWork1/FileChoose.java
+++ src/classWork1/FileChoose.java
@@ -1,32 +1,59 @@
  package classWork1;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 public class FileChoose extends JFrame {
+
+private BufferedImage originalBI;
 public FileChoose()
 {
-	setSize(400,400);
+
+	setSize(400,200);
 	setLocation(600,300);
-	setVisible(true);
-	setDefaultCloseOperation(EXIT_ON_CLOSE);
 	
+	setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	
+	JPanel da= new JPanel();
+	JButton b1 = new JButton("Open File")  ;
 	JFileChooser fc = new JFileChooser();
+	da.add(b1);
+	add(da,BorderLayout.WEST);
 	fc.setCurrentDirectory(new File("c:\\temp"));
 	  FileNameExtensionFilter filter = new FileNameExtensionFilter(
 		        "JPG & GIF Images", "jpg", "gif");
 		    fc.setFileFilter(filter);
-	JButton b1 = new JButton("Open File")  ;
 	
+	setVisible(true);
 	b1.addActionListener(new ActionListener(){
 		
 		public void actionPerformed(ActionEvent e){
-			int returnValue = fc.Been there before(this);
-			
+			int returnValue = fc.showOpenDialog(null);
+			if (returnValue == JFileChooser.APPROVE_OPTION){
+				try{
+					originalBI = ImageIO.read(fc.getSelectedFile());
+					
+				}catch(IOException ioe){
+					
+				}
+			}
+			else{
+				
+			}
+			;
 	}});
 	
-	add(b1);
+
+	
+}
+public static void main(String [] args){
+	new FileChoose();
 }
 }
