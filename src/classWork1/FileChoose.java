45bfa7931bc037ba558d87abc55e3ef6396c86f5
bernard <bernard@192.168.0.18>
2017-09-17 21:34:13 -0500

    done2

--- src/classWork1/FileChoose.java
+++ src/classWork1/FileChoose.java
@@ -1,5 +1,10 @@
-package classWork1;
-import javax.swing.JFrame;
+ package classWork1;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 public class FileChoose extends JFrame {
 public FileChoose()
 {
@@ -8,6 +13,20 @@ public FileChoose()
 	setVisible(true);
 	setDefaultCloseOperation(EXIT_ON_CLOSE);
 	
+	JFileChooser fc = new JFileChooser();
+	fc.setCurrentDirectory(new File("c:\\temp"));
+	  FileNameExtensionFilter filter = new FileNameExtensionFilter(
+		        "JPG & GIF Images", "jpg", "gif");
+		    fc.setFileFilter(filter);
+	JButton b1 = new JButton("Open File")  ;
 	
+	b1.addActionListener(new ActionListener(){
+		
+		public void actionPerformed(ActionEvent e){
+			int returnValue = fc.Been there before(this);
+			
+	}});
+	
+	add(b1);
 }
 }
